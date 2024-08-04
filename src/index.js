df78ec922a2a66672e1f13f86cc723d47793f3b0
jyanokye <jamesyawanokye17@gmail.com>
2024-08-04 22:54:57 +0000

    Done

--- src/index.js
+++ src/index.js
@@ -1,11 +1,18 @@
+import {StrictMode} from 'react';
+import {createRoot} from 'react-dom/client';
 
-import React from 'react';
-import ReactDOM from 'react-dom';
 import App from './App';
 
-ReactDOM.render(
-  <React.StrictMode>
+// 👇️ IMPORTANT: use the correct ID of your root element
+// This is the ID of the div in your index.html file
+const rootElement = document.getElementById('root');
+const root = createRoot(rootElement);
+
+// 👇️ if you use TypeScript, add non-null (!) assertion operator
+// const root = createRoot(rootElement!);
+
+root.render(
+  <StrictMode>
     <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+  </StrictMode>,
 );
